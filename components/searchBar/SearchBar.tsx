d0aa3a5df5de969b56cad443a22c21a8a1f62f60
Arthur Lopes <arthurlopr12@gmail.com>
2023-08-27 19:03:12 -0300

    feat: add route to search params

--- components/searchBar/SearchBar.tsx
+++ components/searchBar/SearchBar.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import React, { useState } from 'react'
+import { useRouter } from 'next/navigation';
 import SearchManufacturer from '../searchManufacturer/SearchManufacturer'
 import SearchButton from '../seachButton/SearchButton';
 import Image from 'next/image';
@@ -8,8 +9,37 @@ import Image from 'next/image';
 const SearchBar = () => {
   const [manufacturer, setManufacturer] = useState('');
   const [model, setModel] = useState('');
+  const router = useRouter();
 
-  const handleSearch = () => { }
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if ( manufacturer === '' || model === '' ) {
+      return alert('Please fill in all fields');
+    }
+
+    updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase());
+  }
+
+  const updateSearchParams = (model: string, manufacturer: string) => {
+    const searchParams = new URLSearchParams(window.location.search);
+
+    if (model) {
+      searchParams.set('model', model);
+    } else {
+      searchParams.delete('model');
+    }
+
+    if (manufacturer) {
+      searchParams.set('manufacturer', manufacturer);
+    } else {
+      searchParams.delete('manufacturer');
+    }
+
+    const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+
+    router.push(newPathname);
+  }
 
   return (
     <form
