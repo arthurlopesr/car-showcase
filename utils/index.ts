636775e3eeeb0cd94da374ae18436fe757c4db70
Arthur Lopes <arthurlopr12@gmail.com>
2023-08-27 16:54:09 -0300

    feat: add new functions in request api

---
+++ utils/index.ts
@@ -0,0 +1,30 @@
+export async function fetchCars() {
+  const headers = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '36b47839e4mshcfa0984213048dap1fa2dcjsnc0f446faa928',
+      'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+    }
+  }
+
+  const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', headers);
+
+  const result = await response.json();
+
+  return result;
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50; // Base rental price per day in dollars
+  const mileageFactor = 0.1; // Additional rate per mile driven
+  const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+  // Calculate additional rate based on mileage and age
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  // Calculate total rental rate per day
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
