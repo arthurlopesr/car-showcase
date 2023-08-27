56b1ba0916f1c26a82186515f52c35c687fdf171
Arthur Lopes <arthurlopr12@gmail.com>
2023-08-27 18:55:17 -0300

    feat: add taguan filter

--- components/searchBar/SearchBar.tsx
+++ components/searchBar/SearchBar.tsx
@@ -2,9 +2,12 @@
 
 import React, { useState } from 'react'
 import SearchManufacturer from '../searchManufacturer/SearchManufacturer'
+import SearchButton from '../seachButton/SearchButton';
+import Image from 'next/image';
 
 const SearchBar = () => {
   const [manufacturer, setManufacturer] = useState('');
+  const [model, setModel] = useState('');
 
   const handleSearch = () => { }
 
@@ -18,7 +21,31 @@ const SearchBar = () => {
           manufacturer={manufacturer}
           setManufacturer={setManufacturer}
         />
+        <SearchButton
+          otherClasses='sm:hidden'
+        />
       </div>
+
+      <div className='searchbar__item'>
+        <Image
+          src='/model-icon.png'
+          alt='Model'
+          width={25}
+          height={25}
+          className='absolute w-[20px] h-[20px] ml-4'
+        />
+        <input
+          type="text"
+          name='model'
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder='Tiguan'
+          className='searchbar__input'
+        />
+
+        <SearchButton otherClasses='sm:hidden' />
+      </div>
+      <SearchButton otherClasses='max-sm:hidden' />
     </form>
   )
 }
